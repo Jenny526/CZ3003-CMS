c2a80b62b68bdeecf431fe600c45f6528b8d8166
taozi <gjintao95@gmail.com>
2015-03-21 20:23:35 +0800

    air pollution API modified

--- Public UI/CMS/lib/airPollutionAPI.js
+++ Public UI/CMS/lib/airPollutionAPI.js
@@ -1,8 +1,19 @@
 $(function(){
-	var getUrl = "https://query.yahooapis.com/v1/public/yql?q=Select * from html where url='http://www.nea.gov.sg/anti-pollution-radiation-protection/air-pollution-control/psi/psi-readings-over-the-last-24-hours' and xpath='//*[@id=\"main\"]/div[1]/div/div[2]/table/tbody/tr[7]/td[13]'&format=json&diagnostics=true&callback=";
+	var lastHour = getLastHour();
+	var tr;
+
+	if(lastHour <= 12){
+		tr = 7;
+	}else{
+		tr = 14;
+		lastHour = lastHour - 12;
+	}
+	td = lastHour+1;
+
+	var getUrl = "https://query.yahooapis.com/v1/public/yql?q=Select * from html where url='http://www.nea.gov.sg/anti-pollution-radiation-protection/air-pollution-control/psi/psi-readings-over-the-last-24-hours' and xpath='//*[@id=\"main\"]/div[1]/div/div[2]/table/tbody/tr[" + tr + "]/td[" + td + "]/strong'&format=json&diagnostics=true&callback=";
 
 	$.get(getUrl, function(data){
-		var results = data.query.results.td.content;
+		var results = data.query.results.strong.content;
 		var PSI = results;
 		PSI = PSI.trim();
 		var descriptor = getAirQualityDescriptor(PSI);
