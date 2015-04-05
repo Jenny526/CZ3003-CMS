c66ab19f1f9abce36a3ed7c3219e591cbe0ca7f9
Mersin <xmxurey@users.noreply.github.com>
2015-04-05 13:49:48 +0800

    add persistence

--- Server/app/models/Agency.java
+++ Server/app/models/Agency.java
@@ -1,10 +1,16 @@
 package models;
 
+import play.db.ebean.Model;
+import javax.persistence.Id;
+
+
 /**
  * Created by mersin on 5/4/15.
  */
-public class Agency {
+public class Agency extends Model {
+  @Id
   public int id;
+
   public String name;
   public String phone;
   public String mail;
--- Server/app/models/EventTypeAgency.java
+++ Server/app/models/EventTypeAgency.java
@@ -1,9 +1,15 @@
 package models;
 
+import play.db.ebean.Model;
+import javax.persistence.*;
+
+
 /**
  * Created by mersin on 5/4/15.
  */
-public class EventTypeAgency {
+public class EventTypeAgency extends Model {
+  @Id
   public int agencyID;
+
   public int eventTypeID;
 }
