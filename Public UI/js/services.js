2a49d16f383b4f9f07c1f1c9a2d234b209dd39cd
Eleven <elevenloveseleven>
2015-04-08 00:35:09 +0800

    RESTful client to replace http

---
+++ Public UI/js/services.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Boss on 8/4/15.
+ */
+'use strict';
+
+/* Services */
+
+var cmsServices = angular.module('cmsServices', ['ngResource']);
+
+cmsServices.factory('Crisis', ['$resource',
+    function($resource){
+        return $resource('test/:crisisId.json', {}, {
+            query: {method:'GET', params:{crisisId:'test'}, isArray:true}
+        });
+    }]);
