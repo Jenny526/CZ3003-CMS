7c04eb9fcff1aca968665ab2e334e6655e611955
LIU YIKUN <yikolyk@gmail.com>
2015-04-09 23:19:58 +0800

    connect with map

--- Public UI/js/services.js
+++ Public UI/js/services.js
@@ -9,7 +9,7 @@ var cmsServices = angular.module('cmsServices', ['ngResource']);
 
 cmsServices.factory('Crisis', ['$resource',
     function($resource){
-        return $resource('//localhost:63342/Public%20UI/test/:crisisId.json', {}, {
+        return $resource('E:/test/:crisisId.json', {}, {
             query: {method:'GET', params:{crisisId:'test'}, isArray:true}
         });
     }]);
