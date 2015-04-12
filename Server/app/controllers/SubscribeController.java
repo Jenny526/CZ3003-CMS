fec74f96dce3ad0992c9758af544598fe15fcefc
taozi <gjintao95@gmail.com>
2015-04-12 17:45:01 +0800

    subscribe modified

--- Server/app/controllers/SubscribeController.java
+++ Server/app/controllers/SubscribeController.java
@@ -4,7 +4,6 @@ package controllers;
  * Created by Jintao on 2015-04-05.
  */
 
-
 import models.*;
 
 import play.data.DynamicForm;
@@ -21,36 +20,9 @@ import static play.libs.Json.toJson;
 public class SubscribeController extends Controller{
     private static Result redirect;
 
-    public static Result index() {
-        return ok(index.render("yes"));
-    }
-
-
     public static Result addEvent(){
-        DynamicForm requestData = Form.form().bindFromRequest();
+       JsonNode json = request().body().asJson();
+       
 
-        String subscriberName = requestData.get("subscriberName");
-        String subscriberPhoneNumber = requestData.get("subscriberMobileNumber");
-        String subscriberLocation = requestData.get("subscriberLocation");
-        String subscriberEmail = requestData.get("subscriberEmail");
-
-        Subscriber newSubscriber = new Subscriber();
-
-        newSubscriber.setId();// unfinished, to generate eventID
-
-        newSubscriber.setSubscriberName(subscriberName);
-        newSubscriber.setSubscriberPhoneNumber(subscriberPhoneNumber);
-        newSubscriber.setSubscriberLocation(subscriberLocation);
-        newSubscriber.setSubscriberEmail(subscriberEmail);
-        newSubscriber.save();
-
-        return redirect(routes.CallOperatorController.index());
-
-    }
-
-    //get event
-    public static Result getEvents(){
-        List<Event> events = new Model.Finder(String.class, Event.class).all();
-        return ok(toJson(events));
     }
 }
