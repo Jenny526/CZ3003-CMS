76f889215c640595fd2edf2b3978af9f0842b3f7
taozi <gjintao95@gmail.com>
2015-04-15 21:46:31 +0800

    code clean up

--- Central Control/handlers/event.js
+++ Central Control/handlers/event.js
@@ -18,3 +18,25 @@ exports.getEventHandler = function(req, res){
 	    res.send(rows);
 	});
 }
+
+exports.updateEventHandler = function(req, res){
+	var event_id = req.params.id;
+	var event_Type = req.body.event_Type;
+	var priority = req.body.priority;
+	var location = req.body.location;
+	var longitude = req.body.longitude;
+	var latitude = req.body.latitude;
+	var status = req.body.status;
+	var reporter_id = req.body.reporter_id;
+	var description = req.body.description;
+	var query = "UPDATE event SET ='event_Type=" + event_Type + "', priority='" + priority + "', location='" + location + "', longitude='" + 
+				longitude + "', latitude='" + latitude + "', status='" + status + "', reporter_id='" + reporter_id + "', description='" + 
+				description + "' WHERE event_id='" + event_id + "'";
+
+	connection.query(query, function(err, rows, fields){
+	    if(err){
+	       	throw err;
+	    }
+	    res.send("OK");
+	});
+}
